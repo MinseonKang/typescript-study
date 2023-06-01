a496be38177fccc79d1ae936f720760d579a099e
MinSeon <s_2___@naver.com>
2023-06-01 21:58:00 +0900

    Typescript 배열과 튜플

---
+++ section2/dist/chapter1.js
@@ -0,0 +1,25 @@
+// number
+let num1 = 123;
+let num2 = -123;
+let num3 = 0.123;
+let num4 = -0.123;
+let num5 = Infinity;
+let num6 = -Infinity;
+let num7 = NaN;
+// string
+let str1 = "hello";
+let str2 = `hello`;
+let str3 = `hello %${num1}`;
+// boolean
+let bool1 = true;
+let bool2 = false;
+// null
+let null1 = null;
+// undefined
+let unde1 = undefined;
+// let numA: number = null; // strictNullChecks 옵션 설정 (null 타입이 아님에도 null 값 할당 가능)
+// 리터럴(값) Type
+let numA = 10;
+let strA = "hello";
+let boolA = true;
+export {};
